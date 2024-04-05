2cd13d568938e31d82f8a1f722e2ea80b9e845ab
Sohail Khan <133193585+re-sohail@users.noreply.github.com>
2024-04-05 15:34:49 +0000

    function

---
+++ Function/function.js
@@ -0,0 +1,48 @@
+let a = 3 
+let b = 5
+let sum = a + b
+
+console.log(3 + 5)
+
+let a = 5
+let b = 3
+console.log(a)
+a = 7
+b = 2
+
+console.log(a+b)
+
+function demo(){
+    let a = 2
+    let b = 3
+    console.log(a + b)
+}
+
+demo()
+
+function demo2(){
+    let a = 3
+    let b = 3
+    return a + b
+}
+console.log(demo2())
+
+// Arrow Function
+let a = ()=>{
+    let b = 3 + 5
+    console.log(b)
+}
+console.log(a)
+
+
+
+
+document.addEventListener("click", ()=>{
+    
+})
+document.addEventListener("hover", ()=>{
+
+})
+document.addEventListener("mouseleave", ()=>{
+
+})
